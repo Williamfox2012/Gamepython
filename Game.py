f9ebd86884bd87ffac23762392c01f2d3f268cf4
Williamfox2012 <141017859+Williamfox2012@users.noreply.github.com>
2024-03-14 21:26:41 +0330

    first commit

--- Game.py
+++ Game.py
@@ -1,28 +1,47 @@
 import curses
+import random
+
 stdscr = curses.initscr()
+curses.noecho()
+curses.cbreak()
+stdscr.keypad(True)
 
 maxl = curses.LINES - 1
 maxc = curses.COLS - 1
 
-
 world = []
-
+plyayer_l = plyayer_c = 0
 def init():
+    global plyayer_c, plyayer_l
     for i in range(maxl):
-        world[i] = []
+        world.append([])
         for j in range(maxc):
-            world[i][j] = '.'
+            world[i].append(' ' if random.random() > 0.03 else '.')
+    plyayer_l = random.randint(0, maxl)
+    plyayer_c = random.randint(0, maxc)
+
+
+
 
 def draw():
     for i in range(maxl):
         for j in range(maxc):
             stdscr.addch(i, j, world[i][j])
+    stdscr.addch(plyayer_l, plyayer_c, 'x')    
+    stdscr.refresh()
 
-curses.noecho()
-curses.cbreak()
-stdscr.keypad(True)
+def move(c):
+    ''' get one of asdw and moved towrad that direction'''
+    pass
 
+init()
 
+playgin = True
+while playgin:
+    c = stdscr.getkey()
+    if c ["asdw"]:
+        move(c)
+    elif c == 'q':
+        playding = False
+    draw()  
 
-stdscr.refresh()
-stdscr.getkey()
